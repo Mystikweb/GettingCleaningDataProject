1d617330c7242ffd6aee3185cd52784e71336264
Christopher Hair <mystikweb@live.ca>
2017-04-29 16:49:13 -0400

    Getting test data setup

--- src/CourseProject/run_analysis.R
+++ src/CourseProject/run_analysis.R
@@ -1,14 +1,24 @@
-# Create a data folder to store the unzipped files
-if (!file.exists("./data")) {
-    dir.create("data")
-}
-
-# Create a temporary file to hold the download
+# Create a temp file to hold the download
 zipTemp <- tempfile()
 
 # Download the required zip file into the temp folder
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile = zipTemp)
 
-# Unzip the file into the data directory
-unzip(zipTemp, exdir = "./data");
+# From the README.txt in the zip file extract the data from these paths
+activity_label_data <- read.table(unz(zipTemp, "UCI HAR Dataset/activity_labels.txt"),
+                                  col.names = c("ActivityId", "ActivityName"))
+
+test_subject_data <- read.table(unz(zipTemp, "UCI HAR Dataset/test/subject_test.txt"),
+                                col.names = c("SubjectId"))
+test_y_data <- read.table(unz(zipTemp, "UCI HAR Dataset/test/y_test.txt"))
+test_x_data <- read.table(unz(zipTemp, "UCI HAR Dataset/test/X_test.txt"))
+
+#train_y_file <- "UCI HAR Dataset/train/y_train.txt"
+#train_x_file <- "UCI HAR Dataset/train/X_train.txt"
+
+#train_y_data <- read.table(unz(zipTemp, train_y_file))
+#train_x_data <- read.table(unz(zipTemp, train_x_file))
+
+# Clean up the temp file
 unlink(zipTemp)
+rm(zipTemp)
